104b993d246d64638df7395c3d8405c96fdb31b4
wcrd <11898748+wcrd@users.noreply.github.com>
2023-01-29 15:54:51 -0700

    - Add validation library - Add function to process shapes and return results - Add shape files - Update logger to take additional options - Update tll_loader to be more generic and take additional options - Update buttons

--- src/lib/js/processing/ttl_loader.js
+++ src/lib/js/processing/ttl_loader.js
@@ -10,50 +10,52 @@ import BrickOntologyPath from '$lib/data/brick.ttl'
 const parser = new N3.Parser()
 
 
-async function ttl_loader(file, n3_store){
+async function ttl_loader(file, n3_store, { load_ontologies = true, log_messages = true } = {}){
 
     // Load user model
     // console.log("## Loading User submitted model")
-    logger("## Loading User submitted model")
+    logger("## Loading User submitted model", {suppress: !log_messages})
     const user_model_text = await fileHandler(file);
     await parser.parse(user_model_text, (error, quad) => {
         if (quad) {
             n3_store.addQuad(quad.subject, quad.predicate, quad.object, defaultGraph())
         } else {
             // console.log("# User model loading complete.");
-            logger("# User model loading complete.");
+            logger("# User model loading complete.", {suppress: !log_messages});
         }
     });
 
-    // Load Brick and Switch ontologies
-    // SWITCH
-    // console.log("## Loading Switch Ontology")
-    logger("## Loading Switch Ontology")
-    const SwitchOntologyFile = await fetch(SwitchOntologyPath)
-    const SwitchOntology = await SwitchOntologyFile.text()
-    const SwitchGraph = namedNode("https://graph.com/switch#")
-    await parser.parse(SwitchOntology, (error, quad) => {
-        if (quad) {
-            n3_store.addQuad(quad.subject, quad.predicate, quad.object, SwitchGraph)
-        } else {
-            // console.log("# Switch Ontology loading complete.");
-            logger("# Switch Ontology loading complete.");
-        }
-    })
-    // BRICK
-    // console.log("## Loading Brick Ontology")
-    logger("## Loading Brick Ontology")
-    const BrickOntologyFile = await fetch(BrickOntologyPath)
-    const BrickOntology = await BrickOntologyFile.text()
-    const BrickGraph = namedNode("https://graph.com/brick#")
-    await parser.parse(BrickOntology, (error, quad) => {
-        if (quad) {
-            n3_store.addQuad(quad.subject, quad.predicate, quad.object, BrickGraph)
-        } else {
-            // console.log("# Brick Ontology loading complete.");
-            logger("# Brick Ontology loading complete.");
-        }
-    })
+    if (load_ontologies){
+        // Load Brick and Switch ontologies
+        // SWITCH
+        // console.log("## Loading Switch Ontology")
+        logger("## Loading Switch Ontology", {suppress: !log_messages})
+        const SwitchOntologyFile = await fetch(SwitchOntologyPath)
+        const SwitchOntology = await SwitchOntologyFile.text()
+        const SwitchGraph = namedNode("https://graph.com/switch#")
+        await parser.parse(SwitchOntology, (error, quad) => {
+            if (quad) {
+                n3_store.addQuad(quad.subject, quad.predicate, quad.object, SwitchGraph)
+            } else {
+                // console.log("# Switch Ontology loading complete.");
+                logger("# Switch Ontology loading complete.", {suppress: !log_messages});
+            }
+        })
+        // BRICK
+        // console.log("## Loading Brick Ontology")
+        logger("## Loading Brick Ontology", {suppress: !log_messages})
+        const BrickOntologyFile = await fetch(BrickOntologyPath)
+        const BrickOntology = await BrickOntologyFile.text()
+        const BrickGraph = namedNode("https://graph.com/brick#")
+        await parser.parse(BrickOntology, (error, quad) => {
+            if (quad) {
+                n3_store.addQuad(quad.subject, quad.predicate, quad.object, BrickGraph)
+            } else {
+                // console.log("# Brick Ontology loading complete.");
+                logger("# Brick Ontology loading complete.", {suppress: !log_messages});
+            }
+        })
+    }
 
     return true
 }
